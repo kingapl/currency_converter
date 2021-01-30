7a32d5404d92300d917f69b7924509abc8b8a83a
kingapl <kingapluciennik@wp.pl>
2021-01-30 13:52:56 +0100

    Changed look app.

--- currency_converter.py
+++ currency_converter.py
@@ -10,35 +10,35 @@ class CurrencyConverter(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.label = tk.Label(self, text="Convert currency")
-        self.label.grid(row=0, column=0, columnspan=4)
+        self.label = tk.Label(self, text="Convert currency", font="Arial 20")
+        self.label.grid(row=0, column=0, columnspan=4, pady=20)
 
-        self.amount_label = tk.Label(self, text="Amount:")
+        self.amount_label = tk.Label(self, text="Amount:", font="Arial 12")
         self.amount_label.grid(row=1, column=0)
 
-        self.currency_label = tk.Label(self, text="Currency:")
+        self.currency_label = tk.Label(self, text="Currency:", font="Arial 12")
         self.currency_label.grid(row=1, column=1)
 
-        self.exchange_label = tk.Label(self, text="Exchange to:")
+        self.exchange_label = tk.Label(self, text="Exchange to:", font="Arial 12")
         self.exchange_label.grid(row=1, column=2)
 
-        self.amount_entry = tk.Entry(self)
-        self.amount_entry.grid(row=2, column=0)
+        self.amount_entry = tk.Entry(self, font="Arial 12", width=10)
+        self.amount_entry.grid(row=2, column=0, padx=10)
 
-        self.currency_entry = tk.Entry(self)
-        self.currency_entry.grid(row=2, column=1)
+        self.currency_entry = tk.Entry(self, font="Arial 12", width=10)
+        self.currency_entry.grid(row=2, column=1, padx=10)
 
-        self.exchange_entry = tk.Entry(self)
-        self.exchange_entry.grid(row=2, column=2)
+        self.exchange_entry = tk.Entry(self, font="Arial 12", width=10)
+        self.exchange_entry.grid(row=2, column=2, padx=10)
 
-        self.convert = tk.Button(self, text="Convert")
-        self.convert.grid(row=2, column=3)
+        self.convert = tk.Button(self, text="Convert", font="Arial 12")
+        self.convert.grid(row=2, column=3, padx=10)
 
-        self.current_currency = tk.Entry(self)
-        self.current_currency.grid(row=3, column=1)
+        self.current_currency = tk.Entry(self, font="Arial 12", width=10)
+        self.current_currency.grid(row=3, column=1, padx=10, pady=10)
 
-        self.exchanged = tk.Entry(self)
-        self.exchanged.grid(row=3, column=2)
+        self.exchanged = tk.Entry(self, font="Arial 12", width=10)
+        self.exchanged.grid(row=3, column=2, padx=10, pady=10)
 
 root = tk.Tk()
 currency_converter = CurrencyConverter(master=root)
